a3202f261b03ea8e8062dfc8e85d359c775fb614
raibarra04 <ribarra764@gmail.com>
2025-08-31 21:07:11 -0700

    accept input from command line from user

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
 def main():
-    load_dotenv()
-    api_key = os.environ.get("GEMINI_API_KEY")
-    client = genai.Client(api_key=api_key)
-    response = client.models.generate_content(
-            model='gemini-2.0-flash-001', 
-            contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.')
-
-    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-    print("Response tokens:", response.usage_metadata.candidates_token_count)
-    print("Response:")
-    print(response.text)
-
+    if len(sys.argv) > 1:
+        print(sys.argv[1])
+        load_dotenv()
+        api_key = os.environ.get("GEMINI_API_KEY")
+        client = genai.Client(api_key=api_key)
+        response = client.models.generate_content(
+                model='gemini-2.0-flash-001', 
+                contents=sys.argv[1]
+                )
+        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+        print("Response tokens:", response.usage_metadata.candidates_token_count)
+        print("Response:")
+        print(response.text)
+    
+    else:
+        print("No text argument provided, input format: uv run main.py <string phrase>")
+        sys.exit(1)
 
 if __name__ == "__main__":
     main()
