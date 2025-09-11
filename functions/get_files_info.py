ead113ae7419aa3725be3ceb746318817cbb6d8f
raibarra04 <ribarra764@gmail.com>
2025-09-10 21:40:46 -0700

    modified test file, fixed formatting

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,5 +1,4 @@
 import os
-import sys
 
 def get_files_info(working_directory, directory="."):
     try:
@@ -16,22 +15,13 @@ def get_files_info(working_directory, directory="."):
             if directory == '.':
                 dir_arg = 'current'
 
-            print(f"Result for {dir_arg} directory:")
             for item in os.listdir(dir_abs_path):
                 file_abs_path = os.path.join(dir_abs_path, item)
                 file_size = os.path.getsize(file_abs_path)
                 is_dir = os.path.isfile(file_abs_path)
-                print(f'   - {item}: file-size={file_size} bytes, is_dir={is_dir}')
-
+                print(f'- {item}: file-size={file_size} bytes, is_dir={is_dir}')
+        return 0
     
     except Exception as e:
-        print(f"Result for {directory} directory:")
-        print(f'   Error: {str(e)}')
+        print(f'Error: {str(e)}')
         return 1
-
-
-get_files_info("../calculator", ".")
-get_files_info("../calculator", "pkg")
-get_files_info("../calculator", "/bin")
-get_files_info("../calculator", "../")
-get_files_info("../calculator", "wumbo")
