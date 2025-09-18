bd929a52cca016f636488d428f792a832720bd57
raibarra04 <ribarra764@gmail.com>
2025-09-17 23:57:15 -0700

    refactoring, and added looping for agent

--- main.py
+++ main.py
@@ -5,14 +5,18 @@ from google import genai
 from google.genai import types
 
 from prompts import system_prompt
-from call_function import available_functions
-from functions.call_function import call_function
+from call_function import call_function, available_functions
 
 def main():
     load_dotenv()
-    args = sys.argv[1:]
-    verbose = False
+    
+    verbose = "--verbose" in sys.argv
+    args = []
     agent_response = ''
+    
+    for arg in sys.argv[1:]:
+        if not arg.startswith("--"):
+            args.append(arg)
 
     if not args:
         print("Code Assistant")
@@ -23,40 +27,55 @@ def main():
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
     
-    if "--verbose" in args:
-        verbose = True
-        args.remove("--verbose")
-
     user_prompt = " ".join(args)
+
+    if verbose:
+        print(f"User prompt: {user_prompt}\n")
 
     messages = [
         types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
 
-    response = client.models.generate_content(
-            model="gemini-2.0-flash-001",
-            contents=messages,
-            config=types.GenerateContentConfig(
-                tools=[available_functions], system_instruction=system_prompt),
-            )
+    for i in range(20):
+        try:
+            response = client.models.generate_content(
+                    model="gemini-2.0-flash-001",
+                    contents=messages,
+                    config=types.GenerateContentConfig(
+                        tools=available_functions, system_instruction=system_prompt),
+                    )
 
-    if len(response.function_calls) > 0:
-        for function_call in response.function_calls:
-            current_fn_call = call_function(function_call, verbose)
-        
-            if not current_fn_call.parts[0].function_response.response:
-                raise ValueError('Error: retrieving response from called function')
-        
+            if (len(response.candidates) > 0):      
+                for candidate in response.candidates:
+                    messages.append(candidate.content)
+
             if verbose:
-                print(f"-> {current_fn_call.parts[0].function_response.response}")
+                print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+                print("Response tokens:", response.usage_metadata.candidates_token_count)
+            
+            if response.function_calls:
+                function_responses = []
+                for function_call_part in response.function_calls:
+                    function_call_result = call_function(function_call_part, verbose)
+                
+                    if (not function_call_result.parts or not function_call_result.parts[0].function_response):
+                        raise Exception("empty function call result")
+                    if verbose:
+                        print(f"-> {function_call_result.parts[0].function_response.response}")
+                
+                    function_responses.append(function_call_result.parts[0])
 
-            agent_response += current_fn_call.parts[0].function_response.response["result"]
-    else:
-        agent_response += response.text
+                if not function_responses:
+                    raise Exception("no function responses generated, exiting.")
+                messages.append(types.Content(role="user", parts=function_responses))
 
-    print(agent_response)
-    return 0
-
+            else:
+                if response.text:
+                    print(response.text)
+                    break
+                
+        except Exception as e:
+            print(f"Error: something happened: {e}")
 
 
 if __name__ == "__main__":
